b63ca0682e02a3867a3d0d4d68890523f9b6c52b
didi04 <2091499s@student.gla.ac.uk>
2018-08-15 15:52:03 +0100

    added the changes to Cooking Buddy as a result of the pilot study

--- lambda/eu-west-1_cookingBuddy/index.js
+++ lambda/eu-west-1_cookingBuddy/index.js
@@ -5,6 +5,7 @@ var http = require('http');
 var ingredients;
 var recipeIndex = 0;
 var recipeID = 0;
+var speechOutput = "";
 
 
 const APP_ID = "amzn1.ask.skill.794d7ffd-9023-4593-949a-894f0d00da35"; 
@@ -25,9 +26,9 @@ const languageStrings = {
             RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?',
             REQUEST_MESSAGE: 'Here is the recipe I found for you:',
             LIKE_RECIPE_MESSAGE: 'Great! Here are the ingredients for this recipe: ',
-            DISLIKE_RECIPE_MESSAGE: 'Okay. If you do not want to proceed, say Cancel. If you want to proceed, say Next.',
             NEXT_MESSAGE: 'Let\'s try this one: ',
             RECIPE_APPROVAL_MESSAGE:'. Do you like this recipe? ',
+            FINISH_MESSAGE: 'You can ask me for another recipe or say Exit to leave',
         },
     },
     'en-US': {
@@ -46,31 +47,31 @@ const handlers = {
     //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
     // Alexa, ask [my-skill-invocation-name] to (do something)...
     'LaunchRequest': function () {
-        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+        speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again.
         this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
 
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.response.speak(speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
     },
 
      /** Provides a help message to the user (how to use the skill) **/ 
     'AMAZON.HelpIntent': function () {
-        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        speechOutput = this.t('HELP_MESSAGE');
         this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
 
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.response.speak(speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
     },
     /** Allows the user to hear the last message again **/
     'AMAZON.RepeatIntent': function () {
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.response.speak(speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
     },
     /** Allows the user to stop the current interaction **/
     'AMAZON.StopIntent': function () {
-        const speechOutput = this.t('STOP_MESSAGE');
+        speechOutput = this.t('STOP_MESSAGE');
         this.response.speak(speechOutput).listen(speechOutput);
         this.emit(':responseReady');
     },
@@ -83,21 +84,14 @@ const handlers = {
     /** Activates when user dislikes a given recipe and
      * provides them with a new one **/
     'AMAZON.NoIntent': function () {
-        const speechOutput = this.t('DISLIKE_RECIPE_MESSAGE');
-        this.response.speak(speechOutput).listen(speechOutput);
-        this.emit(':responseReady');
-    },   
-
-    'AMAZON.NextIntent': function(){
-        let speechOutput = this.t('NEXT_MESSAGE');
+        speechOutput = this.t('NEXT_MESSAGE');
         recipeIndex++;
         const recipe = getRecipes(ingredients, (response) => { //quer the recipes based on the ingredient(s)
-            let speechOutput = response.recipes[recipeIndex].title; // get the recipe's title
+            speechOutput += response.recipes[recipeIndex].title; // get the recipe's title
             if (speechOutput != null){ // check for a matching recipe
                 speechOutput+= this.t('RECIPE_APPROVAL_MESSAGE');
                 this.response.cardRenderer(speechOutput); // display the recipe on a card 
-                this.response.speak("The best recipe I found is: " 
-                + speechOutput).listen(speechOutput); // respond to the user with the recipe
+                this.response.speak(speechOutput).listen(speechOutput); // respond to the user with the recipe
                 this.emit(':responseReady'); 
                 recipeID = response.recipes[recipeIndex].recipe_id;
             }
@@ -106,18 +100,18 @@ const handlers = {
                 this.emit(':responseReady'); 
             }     
         });
-    },
+    },   
     
     /** Activates when the user likes a given recipe
      * and provides them with the ingredients**/
     'AMAZON.YesIntent': function () {
-        let speechOutput = this.t('LIKE_RECIPE_MESSAGE');
+        speechOutput = this.t('LIKE_RECIPE_MESSAGE');
         getIngredients(recipeID, (responseAPI) => {
             let ingredients = responseAPI.recipe.ingredients;
         for (let i=0; i<ingredients.length - 1;i++){
                 speechOutput+= ingredients[i] + ". "; 
             }
-            this.response.speak(speechOutput);
+            this.response.speak(speechOutput).listen(this.t('FINISH_MESSAGE'));
         this.emit(':responseReady');
         });
     }, 
@@ -138,7 +132,7 @@ const handlers = {
     'RequireRecipeIntent': function(){
         ingredients = this.event.request.intent.slots.ingredient.value; //get the ingredient(s) requested by the user
         const recipe = getRecipes(ingredients, (response) => { //quer the recipes based on the ingredient(s)
-            let speechOutput = response.recipes[recipeIndex].title; // get the recipe's title
+            speechOutput = response.recipes[recipeIndex].title; // get the recipe's title
             if (speechOutput != null){ // check for a matching recipe
                 speechOutput+= this.t('RECIPE_APPROVAL_MESSAGE');
                 this.response.cardRenderer(speechOutput); // display the recipe on a card 
